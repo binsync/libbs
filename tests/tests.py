a1c7024dbb62661b970227650f4bc7c39e643981
Flipout50 <cadensounart@gmail.com>
2023-12-04 21:16:25 -0700

    Basic Function Tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,16 @@ from libbs.api import DecompilerInterface
 
 class TestHeadlessInterfaces(unittest.TestCase):
     def test_ghidra_interface(self):
-        #deci = DecompilerInterface.discover_interface(force_decompiler="ghidra", headless=True)
-        # 1. starts the headless ghidra server
-        # todo: do more stuff here, like set a function name
+        # TODO: Make deci headless
+        deci = DecompilerInterface.discover_interface(force_decompiler="ghidra")
+        main = deci.functions[0x400664]
+        main.name = "main"
+        deci.functions[0x400664] = main
+        assert(deci.functions[0x400664].name == "main")
+        main.stack_vars[-24].name = "input"
+        deci.functions[0x400664] = main
+        assert(deci.functions[0x400664].stack_vars[-24].name == "input")
+        main.args[3].name = "key"
+        deci.functions[0x400664] = main
+        assert(main.args[3].name == "key")
         print("under development")
