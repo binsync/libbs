b4e0971aca1a6c8a6381043e783a245a3f1d12c3
mahaloz <zion@zionbasque.com>
2024-05-13 22:03:29 -0400

    Add artifact to name class dict

--- libbs/artifacts/__init__.py
+++ libbs/artifacts/__init__.py
@@ -7,3 +7,17 @@ from .global_variable import GlobalVariable
 from .patch import Patch
 from .stack_variable import StackVariable
 from .struct import Struct, StructMember
+
+ART_NAME_TO_CLS = {
+    Function.__name__: Function,
+    FunctionHeader.__name__: FunctionHeader,
+    FunctionArgument.__name__: FunctionArgument,
+    StackVariable.__name__: StackVariable,
+    Comment.__name__: Comment,
+    GlobalVariable.__name__: GlobalVariable,
+    Enum.__name__: Enum,
+    Struct.__name__: Struct,
+    StructMember.__name__: StructMember,
+    Patch.__name__: Patch,
+    Decompilation.__name__: Decompilation,
+}
