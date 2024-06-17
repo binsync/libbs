cd24afa8020ae433eaef1187a5af7ab257b59a88
Flipout50 <cadensounart@gmail.com>
2024-06-17 12:18:33 -0700

    Revert broken serialization changes

--- libbs/configuration.py
+++ libbs/configuration.py
@@ -31,9 +31,6 @@ class BSConfig:
             if isinstance(attr_val, pathlib.Path):
                 attr_val = str(attr_val)
 
-            if isinstance(attr_val, dict):
-                attr_val = {key: str(value) for key, value in attr_val.items()}
-
             dump_dict[attr] = attr_val
 
         with open(self.save_location, "w") as fp:
