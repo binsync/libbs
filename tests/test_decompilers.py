5b2044fbd9e3c030907ef08bc10ad1997c4cf23f
Flipout50 <cadensounart@gmail.com>
2024-07-06 03:01:31 -0700

    Fix corrupted fauxware test bin

--- tests/test_decompilers.py
+++ tests/test_decompilers.py
@@ -221,7 +221,7 @@ class TestHeadlessInterfaces(unittest.TestCase):
 
         struct = deci.structs['eh_frame_hdr']
         del deci.structs['eh_frame_hdr']
-        assert struct.name not in deci.structs.keys() and struct not in struct.values
+        assert struct.name not in deci.structs.keys() and struct not in struct.values()
 
 
 if __name__ == "__main__":
