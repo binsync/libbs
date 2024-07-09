452be91fc8be4803caaca89792bac6d53ff45b6e
Flipout50 <cadensounart@gmail.com>
2024-07-08 19:21:02 -0700

    Ensure struct is packed when set from libbs

--- libbs/decompilers/binja/interface.py
+++ libbs/decompilers/binja/interface.py
@@ -439,7 +439,7 @@ class BinjaInterface(DecompilerInterface):
     # structs
     def _set_struct(self, struct: Struct, header=True, members=True, **kwargs) -> bool:
         if header:
-            self.bv.define_user_type(struct.name, binaryninja.Type.structure())
+            self.bv.define_user_type(struct.name, binaryninja.Type.structure(packed=True))
 
         if members:
             # this scope assumes that the type is now defined... if it's not we will error
@@ -457,7 +457,6 @@ class BinjaInterface(DecompilerInterface):
                             bn_type = binaryninja.Type.int(bs_memb.size)
 
                     members.append((bn_type, bs_memb.name))
-
                 s.members = members
 
         return True
