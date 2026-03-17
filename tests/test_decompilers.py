24ee97bf0894d7f9f0085d5921a161ec0dee1e72
mahaloz <zion@zionbasque.com>
2026-03-16 19:21:37 -0700

    again

--- tests/test_decompilers.py
+++ tests/test_decompilers.py
@@ -796,6 +796,7 @@ class TestHeadlessInterfaces(unittest.TestCase):
 
         # trigger a decompilation update indirectly through a decompiled comment
         ida_deci.comments[1821] = Comment(addr=1821, comment="test comment!", func_addr=1821, decompiled=True)
+        ida_deci.shutdown()
         assert event_triggered, "Decompilation change event was not triggered"
 
     def test_ida_segment(self):
